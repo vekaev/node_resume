bf9f14f0606163f469c530860b2a041083fbca76
vekaev <33591998qQ>
2020-08-18 08:31:14 +0300

    upload last changes

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
+const http = require('http'),
+    fs = require('fs'),
+    path = require('path'),
+    urlModule = require('url');
 
 const PORT = process.env.PORT || 3000;
 
 
 const server = http.createServer((req, res) => {
+    const { method, url, headers} = req;
+    const reqUrl = urlModule.parse(url, true);
+
+    switch (method) {
+        case 'GET':
+            console.log(headers['content-type']);
+
+
+            if(reqUrl.query){
+                for (el in reqUrl.query){
+                    console.log(el + ": " + reqUrl.query[el]);
+                }
+            }
+    }
+
+
+
     let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
     let contentType = "text/html";
 
@@ -23,24 +41,26 @@ const server = http.createServer((req, res) => {
             break;
     }
 
-    if (!ext){
-        filePath +='.html'
-    };
+    if (!ext) {
+        filePath += '.html'
+    }
+    ;
 
     fs.readFile(filePath, (err, data) => {
-        if (err){
+        if (err) {
             fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
                 if (err) {
                     res.writeHead(500);
                     res.end("404");
-                }else {
+                } else {
                     res.writeHead(500, {"Content-Type": contentType});
                     res.end(content);
                 }
             });
-        } else{
+        } else {
             res.writeHead(200, {"Content-Type": contentType});
             res.end(data);
-        };
+        }
+        ;
     });
 }).listen(PORT);
